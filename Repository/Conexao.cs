e9dda2ad6b4413c932dd60ba6d58a3549426667f
Willians Freitas <williansvpf@gmail.com>
2025-03-28 13:28:47 -0300

    biblioteca responsavel pela presistencia dos dados

---
+++ Repository/Conexao.cs
@@ -0,0 +1,10 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Repository
+{
+    public class Conexao
+    {
+        private static string connectionString = Environment.GetEnvironmentVariable("connectionContato");
+        public static MySqlConnection GetConnection => new MySqlConnection(connectionString);
+    }
+}
