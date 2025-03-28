d57649a15f40f5eaeab937c46d40ae2de7525925
Willians Freitas <williansvpf@gmail.com>
2025-03-28 20:51:19 -0300

    criado metodo para registrar usuario e a interface correspondente

---
+++ Repository/UsuarioDB.cs
@@ -0,0 +1,41 @@
+﻿using Interfaces;
+using Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UsuarioDB : IUsuarioDB
+    {
+        public bool RegistraUsuario(Usuario usuario)
+        {
+            try
+            {
+                using (var connection = Conexao.GetConnection)
+                {
+                    connection.Open();
+                    string sql = "INSERT INTO usuario VALUES(0, @login,  @nome, @senha, @salt)";
+
+                    using (var command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@login", usuario.Login);
+                        command.Parameters.AddWithValue("@nome", usuario.Nome);
+                        command.Parameters.AddWithValue("@senha", usuario.Senha);
+                        command.Parameters.AddWithValue("@salt", usuario.Salt);
+                        command.ExecuteNonQuery();
+
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
