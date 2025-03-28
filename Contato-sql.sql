3318ce149d827304b42b850de9c37de33acd9e7b
Willians Freitas <williansvpf@gmail.com>
2025-03-28 09:59:17 -0300

    SQL

---
+++ Contato-sql.sql
@@ -0,0 +1,35 @@
+create database Contatos;
+use contatos;
+
+create table Usuario(
+idUsuario int primary key not null auto_increment,
+login varchar(45) not null unique,
+nome varchar(45) not null,
+senha varchar(45) not null,
+salt varchar(45) not null
+);
+
+create table Contato(
+idContato int primary key not null auto_increment,
+nome varchar(45) not null,
+idUsuario int not null,
+foreign key (idUsuario) references Usuario (idUsuario)
+);
+
+create table TipoCOntato(
+idTipoContato int primary key not null auto_increment,
+tipo varchar(45) not null unique,
+decricao varchar(45)
+);
+
+create table EnderecoContato(
+idEnderecoContato int primary key not null auto_increment,
+valor varchar(45) not null,
+principal tinyint not null,
+observacao varchar(45),
+idTipoContato int not null,
+idContato int not null,
+foreign key (idTipoContato) references TipoContato (idTipoContato),
+foreign key (idContato) references Contato (idContato)
+);
+
