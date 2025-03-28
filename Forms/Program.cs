b659b475b0d0a9b51a2ab3e360b2ccac91d6021d
Willians Freitas <williansvpf@gmail.com>
2025-03-28 20:47:43 -0300

    adicionado pacote para lidar com a injeção de dependencias e feito as configurações

--- Forms/Program.cs
+++ Forms/Program.cs
@@ -1,7 +1,13 @@
+using Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+
 namespace Forms
 {
     internal static class Program
     {
+
+        private static IServiceProvider serviceProvider;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -11,7 +17,29 @@ namespace Forms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Main());
+
+            serviceProvider = ConfigureServices();
+
+            var mainForm = serviceProvider.GetRequiredService<Main>();
+
+            Application.Run(mainForm);
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Registrar os formulários e serviços do projeto
+            services.AddScoped<Main>(); // Formulário principal
+            services.AddScoped<Login>(); // Formulário Login
+            services.AddScoped<Registrar>(); // Outro formulário
+            services.AddScoped<IUsuarioDB, UsuarioDB>(); // Repositório
+
+            var provider = services.BuildServiceProvider();
+            services.AddSingleton(provider);
+
+            // Criar e retornar o provedor de serviços
+            return services.BuildServiceProvider();
         }
     }
 }
