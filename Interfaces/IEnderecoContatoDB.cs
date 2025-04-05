db5206f7bda35a91fae1b23a5fb3241c868c3664
Willians Freitas <williansvpf@gmail.com>
2025-04-04 23:31:19 -0300

    adicionado Interface e Repository para EnderecoContatoDB

---
+++ Interfaces/IEnderecoContatoDB.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IEnderecoContatoDB
+    {
+        bool NovoEnderecoContato(EnderecoContato enderecoContato);
+
+        List<EnderecoContato> GetEnderecosContato(int id);
+    }
+}
---
+++ Repository/EnderecoContatoDB.cs
@@ -0,0 +1,56 @@
+﻿using Interfaces;
+using Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EnderecoContatoDB : IEnderecoContatoDB
+    {
+        public List<EnderecoContato> GetEnderecosContato(int id)
+        {
+            List<EnderecoContato> lista = new List<EnderecoContato>();
+            try
+            {
+                using (var connection = Conexao.GetConnection)
+                {
+                    connection.Open();
+                    string sql = "SELECT idEnderecoContato, valor, observacao, idTipoContato FROM  enderecocontato WHERE idContato = @idContato;";
+
+                    using (var command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@idContato", id);
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                lista.Add(new EnderecoContato 
+                                {
+                                    IdEnderecoContato = reader.GetInt32("idEnderecoContato"),
+                                    Valor = reader.GetString("valor"),
+                                    Observacao = reader.GetString("observacao"),
+                                    IdTipoContato = reader.GetInt32("idTipoContato")
+                                });
+                            }
+                            return lista;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool NovoEnderecoContato(EnderecoContato enderecoContato)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
